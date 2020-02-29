e8d8370e6f2bc7aa1afc08c06c4f3905929b82a8
sp1der <null@byte>
2020-02-29 20:13:31 -0300

    bot now sends documents to ipfs-router and retrieves FileHash

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"mime/multipart"
+	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/sp1ddr/punkcontactBot/punkbot"
 )
 
+// IpfsRouter : endpoint
+var IpfsRouter string
+
+// JSONResponse : generic api response
+var JSONResponse map[string]string
+
 func main() {
+
+	IpfsRouter = "http://127.0.0.1:1984"
+
 	apikey := os.Getenv("CONTACTBOT")
 	bot, err := tgbotapi.NewBotAPI(apikey)
 	if err != nil {
@@ -33,11 +49,61 @@ func main() {
 		log.Printf("[%s] %s", update.Message.From.FirstName, update.Message.Text)
 		command := strings.Split(update.Message.Text, " ")[0]
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+		var FileObject tgbotapi.FileConfig
+
+		ChatID := update.Message.Chat.ID
+
+		msg := tgbotapi.NewMessage(ChatID, "")
 		msg.ReplyToMessageID = update.Message.MessageID
 		msg.ParseMode = "Markdown"
 		if update.Message == nil { // ignore any non-Message Updates
 			continue
+		}
+
+		if update.Message.Document != nil {
+			fileID := update.Message.Document.FileID
+			//fileName := update.Message.Document.FileName
+			FileObject.FileID = fileID
+
+			// Dl file from Telegram Server and Save File to Disk
+			file, _ := bot.GetFile(FileObject)
+			uri := file.Link(apikey)
+			filename := strings.Split(file.FilePath, "/")[1]
+			filename = "./storage/" + filename
+
+			if err := punkbot.DownloadFile(filename, uri); err != nil {
+				msg.Text = "Err downloading file from telegram server"
+				bot.Send(msg)
+			}
+
+			f, _ := os.Open(filename)
+			defer f.Close()
+
+			// Send File to IPFS-Router
+			body := &bytes.Buffer{}
+			writer := multipart.NewWriter(body)
+			part, _ := writer.CreateFormFile("file", filepath.Base(f.Name()))
+
+			io.Copy(part, f)
+			writer.Close()
+
+			req, err := http.NewRequest("POST", IpfsRouter+"/ipfs/file", body)
+			req.Header.Add("Content-Type", writer.FormDataContentType())
+
+			client := &http.Client{}
+			response, err := client.Do(req)
+			if err != nil {
+				msg.Text = "Err sending to ipfs-router"
+				bot.Send(msg)
+			} else {
+				body := &bytes.Buffer{}
+				_, _ = body.ReadFrom(response.Body)
+
+				_ = json.NewDecoder(body).Decode(&JSONResponse)
+				msg.Text = "*IPFS Hash:* `" + JSONResponse["hash"] + "`"
+				bot.Send(msg)
+			}
+
 		}
 
 		switch command {
